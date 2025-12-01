0b67b3960792c324a1a093cff4e7777b82988c35
SerifSalem <serif.salem@outlook.com>
2025-12-01 00:49:46 +0000

    Simple App

--- composeApp/src/jvmMain/kotlin/week7_gui/qho543_week7_gui/App.kt
+++ composeApp/src/jvmMain/kotlin/week7_gui/qho543_week7_gui/App.kt
@@ -1,49 +1,17 @@
 package week7_gui.qho543_week7_gui
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import qho543_week7_gui.composeapp.generated.resources.Res
-import qho543_week7_gui.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
 @Composable
-@Preview
+fun HelloWorld() {
+    Text("Hello World fron the Custom Composable!")
+}
+
+@Composable
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        HelloWorld()
     }
 }
--- composeApp/src/jvmMain/kotlin/week7_gui/qho543_week7_gui/main.kt
+++ composeApp/src/jvmMain/kotlin/week7_gui/qho543_week7_gui/main.kt
@@ -3,11 +3,9 @@ package week7_gui.qho543_week7_gui
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+// main.kt
 fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "qho543_week7_gui",
-    ) {
+    Window(onCloseRequest = ::exitApplication, title = "Hello World") {
         App()
     }
 }
