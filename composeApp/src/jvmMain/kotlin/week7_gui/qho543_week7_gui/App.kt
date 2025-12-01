00efd64b8d629fda47c7a13290b6bac7e5cde72e
SerifSalem <serif.salem@outlook.com>
2025-12-01 01:04:41 +0000

    More_Complex_Composable

--- composeApp/src/jvmMain/kotlin/week7_gui/qho543_week7_gui/App.kt
+++ composeApp/src/jvmMain/kotlin/week7_gui/qho543_week7_gui/App.kt
@@ -1,17 +1,29 @@
 package week7_gui.qho543_week7_gui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
+fun TwoTexts() {
+    Column {
+        Text("Hello World! ")
+        Text("Welcome to Compose Multiplatform Development")
+    }
+}
+
+@Composable
 fun HelloWorld() {
-    Text("Hello World fron the Custom Composable!")
+    Text("Hello World from the Custom Composable!")
 }
 
 @Composable
 fun App() {
     MaterialTheme {
-        HelloWorld()
+        Column {
+            HelloWorld()
+            TwoTexts()
+        }
     }
 }
