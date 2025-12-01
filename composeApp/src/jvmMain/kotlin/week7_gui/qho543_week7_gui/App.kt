55d57f0d5623dd5605ecede1b4b9bc16fe4593e1
SerifSalem <serif.salem@outlook.com>
2025-12-01 01:27:08 +0000

    Events_and_State

--- composeApp/src/jvmMain/kotlin/week7_gui/qho543_week7_gui/App.kt
+++ composeApp/src/jvmMain/kotlin/week7_gui/qho543_week7_gui/App.kt
@@ -3,12 +3,26 @@ package week7_gui.qho543_week7_gui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+
+@Composable
+fun GreetingBox() {
+    val nameState = remember { mutableStateOf("") }
+    Column {
+        TextField(value = nameState.value, onValueChange= {
+            nameState.value = it
+        })
+        Text("Hello ${nameState.value}!")
+    }
+}
 
 @Composable
 fun MultiMessage(message: String, nTimes: Int, colour: Color = Color.Black) {
@@ -49,6 +63,7 @@ fun App() {
             TwoTexts()
             TwoTextsStyled()
             MultiMessage("Hello QHO543!", 3, Color.Blue)
+            GreetingBox()
         }
     }
 }
